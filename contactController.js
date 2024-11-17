88bc43faa60a2906e50f3820abc63c5d0d4efce6
Shams Fatima <119777359+shamssfatima@users.noreply.github.com>
2024-11-17 16:26:35 +0530

    Add files via upload

---
+++ contactController.js
@@ -0,0 +1,38 @@
+const Contact = require("../models/Contact");
+
+exports.createContact = async (req, res) => {
+  try {
+    const contact = new Contact(req.body);
+    await contact.save();
+    res.status(201).json(contact);
+  } catch (error) {
+    res.status(400).json({ message: "Error creating contact", error });
+  }
+};
+
+exports.getContacts = async (req, res) => {
+  try {
+    const contacts = await Contact.find();
+    res.json(contacts);
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching contacts", error });
+  }
+};
+
+exports.updateContact = async (req, res) => {
+  try {
+    const contact = await Contact.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    res.json(contact);
+  } catch (error) {
+    res.status(400).json({ message: "Error updating contact", error });
+  }
+};
+
+exports.deleteContact = async (req, res) => {
+  try {
+    await Contact.findByIdAndDelete(req.params.id);
+    res.json({ message: "Contact deleted" });
+  } catch (error) {
+    res.status(500).json({ message: "Error deleting contact", error });
+  }
+};
